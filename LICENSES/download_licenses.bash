c5db16e4c5480a9fed20d147ebabc39c978ba492
hkienle <hkienle@posteo.de>
2023-02-12 17:25:39 +0100

    start licensing scheme with REUSE

---
+++ LICENSES/download_licenses.bash
@@ -0,0 +1,5 @@
+
+# https://github.com/spdx/license-list-data/tree/main/text
+
+# CC Zero (technically no license)
+wget --no-clobber "https://raw.githubusercontent.com/spdx/license-list-data/main/text/CC0-1.0.txt"
